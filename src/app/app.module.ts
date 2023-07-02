115302d2f46605f6cd7796401eb06a5274f0ad80
AbdulArif <abdularif.dgp@gmail.com>
2023-07-02 07:57:39 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnauthComponent } from './unauth/unauth.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnauthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
